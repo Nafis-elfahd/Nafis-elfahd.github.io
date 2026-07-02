ef3722fdadc820095529f73d5b9fbe67c5dc9c25
Nafis-elfahd <m.nafisgayo@gmail.com>
2026-07-02 21:55:02 +0700

    add

---
+++ script.js
@@ -0,0 +1,245 @@
+const marketplace = {
+    "result": [
+        {
+            "title": "Fragments of the Overworld Add-On",
+            "neutralTitle": "Fragments of the Overworld Add-On",
+            "url": "https://www.minecraft.net/en-us/marketplace/pdp/bedrock-productions/fragments-of-the-overworld-add--on/b72b7f7d-0f3c-4671-8716-c3e7d46d86ca",
+            "superhead": "",
+            "description": "An entire album of music added to your game! This is the Fragments of the Overworld album as music discs for jukeboxes.\r\n- 17 new music discs\r\n- Challenge for each disc\r\n- From uncommon to legendary discs\r\n- Jukebox and hopper compatible\r\n- By Bedrock Productions",
+            "author": "Bedrock Productions",
+            "image": "https://xforgeassets002.xboxlive.com/pf-namespace-b63a0803d3653643/0779a018-c289-46d8-a4db-d5bcfbbfcfc1/overworldfragments_Thumbnail_0.jpg",
+            "imageAltText": "Fragments of the Overworld Add-On",
+            "price": 490.0,
+            "rating": 4.2,
+            "listingCategory": "",
+            "tags": [
+                "AddOn",
+                "tag.music",
+                "tag.soundtrack",
+                "tag.discover",
+                "genre.creative"
+            ],
+            "time": 1782838800,
+            "type": "Marketplace",
+            "locale": "en-us",
+            "packType": "AddOn"
+        },
+        {
+            "title": "Mermaid Life Add-On",
+            "neutralTitle": "Mermaid Life Add-On",
+            "url": "https://www.minecraft.net/en-us/marketplace/pdp/noxcrew/mermaid-life-add--on/0b8134ca-c682-422b-9a17-1d4a8f91bb8c",
+            "superhead": "",
+            "description": "Explore the ocean and become a Mermaid!\r\n\r\n- Wear any of 6 Tails and swim along the sea floor!\r\n- Decorate your home with 35+ pieces of furniture and new block sets! \r\n- Ride on Orcas and tame Narwhals and Seahorses\r\n- Fight the Sirens and discover new underwater strongholds\r\n\r\n! Created by Noxcrew",
+            "author": "Noxcrew",
+            "image": "https://xforgeassets001.xboxlive.com/pf-namespace-b63a0803d3653643/d9bb4b59-f786-4822-abbd-d3e153f3e057/MermaidLife_Thumbnail_0.jpg",
+            "imageAltText": "Mermaid Life Add-On",
+            "price": 830.0,
+            "rating": 4.0,
+            "listingCategory": "",
+            "tags": [
+                "AddOn",
+                "tag.vibrantvisualsenhanced",
+                "tag.aquatic",
+                "tag.base",
+                "tag.casual",
+                "tag.furniture",
+                "tag.palace",
+                "tag.princess",
+                "tag.underwater",
+                "tag.mystical_creatures",
+                "tag.vibrantvisuals",
+                "genre.roleplaying_game"
+            ],
+            "time": 1782838800,
+            "type": "Marketplace",
+            "locale": "en-us",
+            "packType": "AddOn"
+        },
+        {
+            "title": "Village Expansion Add-On",
+            "neutralTitle": "Village Expansion Add-On",
+            "url": "https://www.minecraft.net/en-us/marketplace/pdp/the-craft-stars/village-expansion-add--on/76e5152c-ef50-4475-a771-7591f8e869d8",
+            "superhead": "",
+            "description": "Village Expansion Add-On adds 100+ villages, evil bosses, custom villagers, and more to YOUR WORLD!\r\n=> 300+ new structures\r\n=> Naturally generated structures\r\n=> Powerful custom bosses\r\n=> New iron golems\r\n=> Epic achievements and rewards\r\n=> Biome villager professions\r\n=> Build your dream base\r\n=> Custom blocks\r\n=> Survival expansion\r\nAdd this Add-On to YOUR WORLD!",
+            "author": "The Craft Stars",
+            "image": "https://xforgeassets001.xboxlive.com/pf-namespace-b63a0803d3653643/4cafb0ed-78b8-4b78-a58a-32d80975f41a/VillageExpansionAddOn_Thumbnail_0.jpg",
+            "imageAltText": "Village Expansion Add-On",
+            "price": 490.0,
+            "rating": 4.9,
+            "listingCategory": "",
+            "tags": [
+                "AddOn",
+                "tag.vibrantvisualsenhanced",
+                "tag.add_on",
+                "tag.adventures",
+                "tag.boss",
+                "tag.build",
+                "tag.custom_villagers",
+                "tag.furniture",
+                "tag.village",
+                "tag.epic",
+                "tag.vibrantvisuals",
+                "genre.survival"
+            ],
+            "time": 1782838800,
+            "type": "Marketplace",
+            "locale": "en-us",
+            "packType": "AddOn"
+        },
+        {
+            "title": "Zoo Builder Add-On",
+            "neutralTitle": "Zoo Builder Add-On",
+            "url": "https://www.minecraft.net/en-us/marketplace/pdp/meraki/zoo-builder-add--on/d642849b-6425-429e-a2ae-81af646c8ae3",
+            "superhead": "",
+            "description": "Zoo Builder brings your dream to life with over 10 new animals, vehicles, furniture, builds, pre-built habitats, and so much more!\r\n\r\n- 10+ NEW mobs\r\n- Pre-built HABITATS, giftshop, a road building, furniture and more!\r\n- A SAFARI NET and truck to TRANSPORT animals!\r\n- Compatible with other animal Add-Ons\r\n\r\nby Meraki",
+            "author": "Meraki",
+            "image": "https://xforgeassets002.xboxlive.com/pf-namespace-b63a0803d3653643/595b2c3f-04cc-4074-9366-f9f9b91052b5/ZooBuilderAdd-On_Thumbnail_0.jpg",
+            "imageAltText": "Zoo Builder Add-On",
+            "price": 660.0,
+            "rating": 4.5,
+            "listingCategory": "",
+            "tags": [
+                "AddOn",
+                "tag.animal",
+                "tag.animals",
+                "tag.base_building",
+                "tag.building",
+                "tag.city_builder",
+                "tag.custom_mobs",
+                "tag.zoo",
+                "tag.trendy",
+                "genre.roleplaying_game"
+            ],
+            "time": 1782838800,
+            "type": "Marketplace",
+            "locale": "en-us",
+            "packType": "AddOn"
+        },
+        {
+            "title": "Utility Tools Add-On",
+            "neutralTitle": "Utility Tools Add-On",
+            "url": "https://www.minecraft.net/en-us/marketplace/pdp/block-factory/utility-tools-add--on/2f9ba826-14c9-43e2-973e-3364d67ee1c0",
+            "superhead": "",
+            "description": "Adds on 40+ craftable tools! your world UtilityTools Add-On\r\n\r\n+ Backpack, Drill, Chainsaw, Dynamic Light, Warp pad\r\n+ Deepforge, Training dummy, Multitools, XP storage",
+            "author": "Block Factory",
+            "image": "https://xforgeassets002.xboxlive.com/pf-namespace-b63a0803d3653643/72e1a7d4-969b-4cee-808e-024fbb6ad380/UtilityTools_Thumbnail_0.jpg",
+            "imageAltText": "Utility Tools Add-On",
+            "price": 990.0,
+            "rating": 5.0,
+            "listingCategory": "",
+            "tags": [
+                "AddOn",
+                "tag.backpack",
+                "tag.craftable_tools",
+                "tag.custom_tools",
+                "tag.farming",
+                "tag.mining",
+                "tag.toolbox",
+                "tag.utilities",
+                "genre.survival"
+            ],
+            "time": 1782838800,
+            "type": "Marketplace",
+            "locale": "en-us",
+            "packType": "AddOn"
+        },
+        {
+            "title": "Medieval City Defence Add-On",
+            "neutralTitle": "Medieval City Defence Add-On",
+            "url": "https://www.minecraft.net/en-us/marketplace/pdp/mraniman2/medieval-city-defence-add--on/705603e9-f3d1-4cf7-a0c9-da3a939ceb5e",
+            "superhead": "",
+            "description": "Defend your castle, city, or survival base with Medieval City Defence Add-On! Add automatic defense turrets, traps, multi-block structures, ladders, and custom medieval themed building and decorative blocks to your Creative and Survival worlds.",
+            "author": "MrAniman2",
+            "image": "https://xforgeassets002.xboxlive.com/pf-namespace-b63a0803d3653643/327d36c6-c960-4baf-9de2-af680a2306bc/MedievalCityDefence_Thumbnail_0.jpg",
+            "imageAltText": "Medieval City Defence Add-On",
+            "price": 660.0,
+            "rating": 3.8,
+            "listingCategory": "",
+            "tags": [
+                "AddOn",
+                "tag.add_on",
+                "tag.base_building",
+                "tag.craftable_blocks",
+                "tag.custom_blocks",
+                "tag.decorate",
+                "tag.defend",
+                "tag.medieval",
+                "tag.city",
+                "genre.survival"
+            ],
+            "time": 1782838800,
+            "type": "Marketplace",
+            "locale": "en-us",
+            "packType": "AddOn"
+        },
+        {
+            "title": "Productive Bees",
+            "neutralTitle": "Productive Bees",
+            "url": "https://www.minecraft.net/en-us/marketplace/pdp/vatonage/productive-bees/e58a5640-f7db-417f-a31c-f1629a2ff9f5",
+            "superhead": "",
+            "description": "Breed powerful, unique pollinators and upgrade your hives to automate resource production and build the ultimate buzzing hive factory!\r\n\r\n- 50+ Bees",
+            "author": "Vatonage",
+            "image": "https://xforgeassets001.xboxlive.com/pf-namespace-b63a0803d3653643/51014c5a-55c9-4dc2-9272-71cce2c007d3/Thumbnail_0.jpg",
+            "imageAltText": "Productive Bees",
+            "price": 830.0,
+            "rating": 2.3,
+            "listingCategory": "",
+            "tags": [
+                "AddOn",
+                "tag.bees",
+                "tag.factory",
+                "tag.farm",
+                "tag.farming",
+                "tag.resource_management",
+                "tag.utilities",
+                "tag.zoo",
+                "tag.custom_resources",
+                "genre.simulation"
+            ],
+            "time": 1782838800,
+            "type": "Marketplace",
+            "locale": "en-us",
+            "packType": "AddOn"
+        },
+        {
+            "title": "Chaos Cubed: Cursed Cube Add-On",
+            "neutralTitle": "Chaos Cubed: Cursed Cube Add-On",
+            "url": "https://www.minecraft.net/en-us/marketplace/pdp/minecraft/chaos-cubed:-cursed-cube-add--on/5d93dc9a-0dbf-467e-95d2-50c61f5eff13",
+            "superhead": "",
+            "description": "The Sulfur Cubes are multiplying! Meet new bizarre cube variants packed with surprises, from helpful oddities to outright menaces. Discover each cube's unique personality, uncover new plants, and raise the ancient Sulfur Sniffer!",
+            "author": "Minecraft",
+            "image": "https://xforgeassets001.xboxlive.com/pf-namespace-b63a0803d3653643/597d5ee4-97c2-46c3-9544-7204d6d2347b/CursedCubes_Thumbnail_0.jpg",
+            "imageAltText": "Chaos Cubed: Cursed Cube Add-On",
+            "price": 0.0,
+            "rating": 4.7,
+            "listingCategory": "",
+            "tags": [
+                "AddOn",
+                "tag.animated",
+                "tag.block",
+                "tag.custom_blocks",
+                "tag.free",
+                "tag.add_on",
+                "genre.creative"
+            ],
+            "time": 1782320400,
+            "type": "Marketplace",
+            "locale": "en-us",
+            "packType": "AddOn"
+        }
+    ]
+}
+
+
+const marketplaceHtml = document.getElementById('marketplace')
+
+marketplace.result.forEach((data) => {
+    marketplaceHtml.innerHTML += `
+    <div class="card">
+        <img class="main-img" src=${data.image} alt=${data.imageAltText}>
+        <div class="card-title">
+            <h1 class="size">${data.title}</h1>
+        </div>
+        <h2>${data.description}</h2>
+    </div>`
+})
